3909cca2f0bce17cee45811be0098ede60c94886
VietAnh244 <117790397+NgVietAnh24@users.noreply.github.com>
2024-07-04 15:48:49 +0700

    Hoàn thành UI ChatPage

--- lib/Pages/Chat/ChatPage.dart
+++ lib/Pages/Chat/ChatPage.dart
@@ -39,11 +39,93 @@ class ChatPage extends StatelessWidget {
           ),
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            color: Theme.of(context).colorScheme.primaryContainer),
+        child: Row(
+          children: [
+            Container(
+              width: 30,
+              height: 30,
+              child: SvgPicture.asset(
+                AssetsImage.chatMicSvg,
+                width: 25,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: false,
+                  hintText: "Type messange ...",
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              child: SvgPicture.asset(
+                AssetsImage.chatGallarySvg,
+                width: 25,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              child: SvgPicture.asset(
+                AssetsImage.chatSendSvg,
+                width: 25,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Column(
+        child: ListView(
           children: [
-            ChatBubble(),
+            ChatBubble(
+              message: "Where you from ?",
+              imageUrl: "",
+              isComing: true,
+              status: "read",
+              time: "10:10 AM",
+            ),
+            ChatBubble(
+              message: "Where you from ?",
+              imageUrl: "",
+              isComing: false,
+              status: "read",
+              time: "10:10 AM",
+            ),
+            ChatBubble(
+              message: "Where you from ?",
+              imageUrl:
+                  "https://cdn.prod.website-files.com/654366841809b5be271c8358/659efd7c0732620f1ac6a1d6_why_flutter_is_the_future_of_app_development%20(1).webp",
+              isComing: true,
+              status: "read",
+              time: "10:10 AM",
+            ),
+            ChatBubble(
+              message: "Where you from ?",
+              imageUrl:
+                  "https://cdn.prod.website-files.com/654366841809b5be271c8358/659efd7c0732620f1ac6a1d6_why_flutter_is_the_future_of_app_development%20(1).webp",
+              isComing: false,
+              status: "read",
+              time: "10:10 AM",
+            ),
           ],
         ),
       ),
--- lib/Pages/Chat/Widgets/ChatBubble.dart
+++ lib/Pages/Chat/Widgets/ChatBubble.dart
@@ -1,26 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mechat/Config/imges.dart';
 
 class ChatBubble extends StatelessWidget {
-  const ChatBubble({super.key});
+  final String message;
+  final bool isComing;
+  final String time;
+  final String status;
+  final String imageUrl;
+  const ChatBubble(
+      {super.key,
+      required this.message,
+      required this.isComing,
+      required this.time,
+      required this.status,
+      required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      constraints: BoxConstraints(
-        maxWidth: MediaQuery.sizeOf(context).width / 1.3,
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: 20,
       ),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.primaryContainer,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-          bottomLeft: Radius.circular(0),
-          bottomRight: Radius.circular(10),
-        ),
-      ),
-      child: Text(
-        "Sau  dddddddd",
+      child: Column(
+        crossAxisAlignment:
+            isComing ? CrossAxisAlignment.start : CrossAxisAlignment.end,
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.sizeOf(context).width / 1.3,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              borderRadius: isComing
+                  ? BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(0),
+                      bottomRight: Radius.circular(10),
+                    )
+                  : BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(0),
+                    ),
+            ),
+            child: imageUrl == ""
+                ? Text(message)
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(imageUrl)),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(message),
+                    ],
+                  ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            mainAxisAlignment:
+                isComing ? MainAxisAlignment.start : MainAxisAlignment.end,
+            children: [
+              isComing
+                  ? Text(
+                      time,
+                      style: Theme.of(context).textTheme.labelMedium,
+                    )
+                  : Row(
+                      children: [
+                        Text(
+                          time,
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SvgPicture.asset(
+                          AssetsImage.chatStatusSvg,
+                          width: 20,
+                        ),
+                      ],
+                    ),
+            ],
+          ),
+        ],
       ),
     );
   }
